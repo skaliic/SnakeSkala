548d1052b3d82c706bc2a298e7ecb520958cf926
Jakub Skála <skalaj24@spstrutnov.cz>
2025-12-04 08:34:56 +0100

    Pokrok v Game1.cs

--- Snake_soutez/Game1.cs
+++ Snake_soutez/Game1.cs
@@ -9,6 +9,73 @@ namespace Snake_soutez
 {
     public class Game1 : Game
     {
+        public enum PowerUpType
+        {
+            SpeedBoost,
+            SlowDown,
+            ScoreDouble,
+            Invincibility
+        }
+
+        public class PowerUp : ICollectible
+        {
+            public Vector2 Position { get; set; }
+            public bool IsActive { get; set; } = true;
+            public float Duration { get; private set; }
+            public PowerUpType Type { get; private set; }
+            private Texture2D texture;
+            private Color color;
+            private Random random = new Random();
+            public PowerUp(PowerUpType type, GraphicsDevice graphicsDevice)
+            {
+                Type = type;
+                // 1x1 pixel textura
+                texture = new Texture2D(graphicsDevice, 1, 1);
+                texture.SetData(new[] { Color.White });
+                // Nastavení barvy + doby trvání
+                switch (type)
+                {
+                    case PowerUpType.SpeedBoost:
+                        color = new Color(0, 255, 255);   // CYAN
+                        Duration = 5f;
+                        break;
+                    case PowerUpType.SlowDown:
+                        color = new Color(0, 120, 255);   // MODRÁ
+                        Duration = 5f;
+                        break;
+                    case PowerUpType.ScoreDouble:
+                        color = new Color(255, 255, 0);   // ŽLUTÁ
+                        Duration = 10f;
+                        break;
+                    case PowerUpType.Invincibility:
+                        color = new Color(255, 0, 255);   // MAGENTA
+                        Duration = 6f;
+                        break;
+                }
+            }
+            public void Spawn(int gridSize, int screenWidth, int screenHeight)
+            {
+                int maxX = screenWidth / gridSize;
+                int maxY = screenHeight / gridSize;
+                Position = new Vector2(
+                    random.Next(maxX) * gridSize,
+                    random.Next(maxY) * gridSize
+                );
+            }
+            public void Apply(Game1 game)
+            {
+                game.ActivatePowerUp(this);
+            }
+            public void Draw(SpriteBatch spriteBatch, int gridSize)
+            {
+                spriteBatch.Draw(
+                    texture,
+                    new Rectangle((int)Position.X, (int)Position.Y, gridSize, gridSize),
+                    color
+                );
+            }
+        }
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -770,6 +837,42 @@ namespace Snake_soutez
                     int boxY = 200;
 
                     // Box s neonovým okrajem
+                    // Box s neonovým okrajem
                     _spriteBatch.Draw(pixelTexture, new Rectangle(boxX, boxY, boxWidth, boxHeight),
                         new Color(0, 0, 0, 220));
-                    _spriteBatch.Draw(pixelTexture, new Rectangle(boxX
+
+                    // Neonový okraj
+                    _spriteBatch.Draw(pixelTexture, new Rectangle(boxX, boxY, boxWidth, 3), new Color(255, 0, 255));
+                    _spriteBatch.Draw(pixelTexture, new Rectangle(boxX, boxY + boxHeight - 3, boxWidth, 3), new Color(255, 0, 255));
+                    _spriteBatch.Draw(pixelTexture, new Rectangle(boxX, boxY, 3, boxHeight), new Color(255, 0, 255));
+                    _spriteBatch.Draw(pixelTexture, new Rectangle(boxX + boxWidth - 3, boxY, 3, boxHeight), new Color(255, 0, 255));
+
+                    // Text
+                    string gameOverText = "GAME OVER";
+                    string scoreText = $"FINAL SCORE: {score}";
+                    string restartText = "PRESS ENTER TO RESTART";
+
+                    Vector2 gameOverSize = font.MeasureString(gameOverText);
+                    Vector2 scoreSize = font.MeasureString(scoreText);
+                    Vector2 restartSize = font.MeasureString(restartText);
+
+                    _spriteBatch.DrawString(font, gameOverText,
+                        new Vector2(boxX + (boxWidth - gameOverSize.X) / 2, boxY + 20),
+                        new Color(255, 0, 255));
+
+                    _spriteBatch.DrawString(font, scoreText,
+                        new Vector2(boxX + (boxWidth - scoreSize.X) / 2, boxY + 55),
+                        new Color(0, 255, 255));
+
+                    _spriteBatch.DrawString(font, restartText,
+                        new Vector2(boxX + (boxWidth - restartSize.X) / 2, boxY + 85),
+                        new Color(255, 255, 0));
+                }
+            }
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
